43ddb5ba220c038ee5f7a99a6201d7190561b049
Selva-Torus <selvakumarg@torus.tech>
2026-06-22 13:16:16 +0530

    flash repo

--- DF/src/envData/envData.module.ts
+++
@@ -1,9 +0,0 @@
-import { Global, Module } from '@nestjs/common';
-import { EnvData } from './envData.service';
-
-@Global()
-@Module({
-  providers: [EnvData],
-  exports: [EnvData],
-})
-export class EnvDataModule {}
